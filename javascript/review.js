b647a2eb00d25280f7342a1f8ad4ad04f4669e7f
Md. Asaduzzaman Shuvo <100297947+Shuvo0725@users.noreply.github.com>
2022-08-07 15:51:54 +0600

    Add files via upload

---
+++ javascript/review.js
@@ -0,0 +1,16 @@
+const tnslider = tns({
+
+    container:".sliderTest",
+    slideBy:1,
+    speed:700,
+    nav:true,
+
+    navPosition:"bottom",
+    autoPlay:true,
+    autoPlayTimeout:7000,
+    autoPlayButtonOutPut:false,
+    controlsContainer:"#controlsTest",
+    prevButton: ".previousTest",
+    nextButton:".nextTest"
+
+})
